d31e58355289a24948ae1e9e2cbc5ad05c66e746
Nathan Bridgewater <nathan@iws.io>
2024-07-29 16:55:14 -0500

    rearranging

--- rollup.config.cjs
+++ rollup.config.cjs
@@ -1,14 +1,11 @@
 const { nodeResolve } = require('@rollup/plugin-node-resolve')
 const commonJs = require('@rollup/plugin-commonjs')
-const { globSync } = require('glob')
-const { join } = require('path')
-
-const actionDirs = globSync('./actions/**/').filter(f => f !== 'actions')
-const inputs = actionDirs.map(dir => ({ [dir.replace(/^actions\//i, '')]: join(dir, 'index.cjs') }))
-const input = Object.assign({}, ...inputs)
+const { join } = require('node:path')
 
 module.exports = {
-	input,
+	input: {
+		'read-package-version': join(__dirname, './read-package-version/index.cjs')
+	},
 	output: {
 		dir: 'dist',
 		fileName: '[name]',
