e31872d717cbdf3851e393a2b56fd1e9dac2688a
Nathan Bridgewater <nathan@iws.io>
2024-07-29 17:37:05 -0500

    Experimental update; TS + ncc

--- read-package-version/index.ts
+++ read-package-version/index.ts
@@ -1,10 +1,10 @@
-const { existsSync, readFileSync } = require('node:fs')
-const { join } = require('node:path')
-const { exportVariable, getInput, setFailed } = require('@actions/core')
+import { existsSync, readFileSync } from 'node:fs'
+import { join } from 'node:path'
+import { exportVariable, getInput, setFailed } from '@actions/core'
 
-function gatherVerisonAndEmit(workspace) {
+function gatherVerisonAndEmit(workspace: string) {
 	console.log(`workspace: ${workspace}`)
-	const projectPath = join(__dirname, '../', workspace, 'package.json') // relative path from working dir to package.json
+	const projectPath = join(__dirname, '../../', workspace, 'package.json') // relative path from working dir to package.json
 
 	if (!existsSync(projectPath)) return console.log(`package.json not found at ${projectPath}`)
 	const contents = readFileSync(projectPath, 'utf8')
@@ -18,6 +18,6 @@ try {
 	// const workspace = process.argv[2]
 	gatherVerisonAndEmit(workspace)
 }
-catch (error) {
+catch (error: any) {
 	setFailed(error.message)
 }
